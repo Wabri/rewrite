a795482f3dd6120e3a4e122d6e39911a3e41505a
IanDuncanT <duncan.ian.t@gmail.com>
2018-12-21 19:01:23 -0600

    Allow for install files to have different extensions

--- yapi.py
+++ yapi.py
@@ -12,12 +12,14 @@ config.read("config.ini")
 operatings = config['OS']['platform']
 cache_location = config['CACHE']['cache_location']
 remote_url = config['REMOTE']['location']
+file_extension = config['REMOTE']['file_extension']
 
 #Function Definitions
 def getFile(operating, package, location):
-    file_url = remote_url + "/" + operating + "/" + package + ".sh"
+    file_url = remote_url + "/" + operating + "/" + package + file_extension
     os.chdir(cache_location)
-    wget.download(file_url, package + ".sh")
+    os.remove(package + file_extension)
+    wget.download(file_url, package + file_extension)
 
 if len(sys.argv) == 3:
     if sys.argv[1] == "install":
