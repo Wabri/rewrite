ce73c485db93e66156101abf9623d9cb2df11bf1
Ian Duncan <duncan.ian.t@gmail.com>
2019-01-09 09:29:26 -0600

    Create download, run, and config methods. (#8)
    download - downloads package
    run - runs package install script
    config - updates config.ini files

--- configuration.py
+++ configuration.py
@@ -1,31 +1,29 @@
-#Imports
-import configparser
-import platform
-import os
+def config_update():
+    import configparser
+    import platform
+    import os
+    if os.path.isfile('config.ini') == False:
+        import shutil
+        shutil.copy('default.ini', 'config.ini')
 
-#Config Updating
-if os.path.isfile('config.ini') == False:
-    import shutil
-    shutil.copy('default.ini', 'config.ini')
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+    if platform.system() == 'Linux':
+        system = 'linux-' #Include dash for distro that is appended to this string
+    elif platform.system() == 'Darwin':
+        system = 'darwin'
+    elif platform.system() == 'Windows':
+        system = 'windows'
 
-if platform.system() == 'Linux':
-    system = 'linux-' #Include dash for distro that is appended to this string
-elif platform.system() == 'Darwin':
-    system = 'darwin'
-elif platform.system() == 'Windows':
-    system = 'windows'
+    if system == 'linux-':
+        import distro
+        switch = {
+            'debian': 'debian',
+            'ubuntu': 'debian'
+        }
+        system += switch.get(distro.id(), 'error')
 
-if system == 'linux-':
-    import distro
-    switch = {
-        'debian': 'debian',
-        'ubuntu': 'debian'
-    }
-    system += switch.get(distro.id(), 'error')
-
-config.set('OS', 'platform', system)
-cfgfile = open("config.ini",'w')
-config.write(cfgfile)
+    config.set('OS', 'platform', system)
+    cfgfile = open("config.ini",'w')
+    config.write(cfgfile)
